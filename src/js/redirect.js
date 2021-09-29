6b0460f34c1ddd578330f600c20fbb17ba190545
doohwancho <doohwancho526@gmail.com>
2021-09-29 18:22:48 +0900

    0.0.6 +: redirect to random leetcode question after 3 second

---
+++ src/js/redirect.js
@@ -0,0 +1,5 @@
+const LEETCODE_RANDOM_PROBLEM_URL = "https://leetcode.com/problems/random-one-question/algorithms";
+
+export function redirectToLeetcode(){
+    window.location.href = LEETCODE_RANDOM_PROBLEM_URL;
+}
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,22 +1,23 @@
+import {redirectToLeetcode} from './redirect.js';
+
 let timeLeftDOM = document.getElementById('time-left');
-let initialTime = 5;
+let initialTime = 3;
 
 function setTime(t){
     timeLeftDOM.innerHTML = t;
 }
 
-function countDown(t){
+function countDown(t, callback){
     if(t < 0){
+        callback();
         return;
     } 
     setTime(t);
     setTimeout(function() {
-        countDown(t-1);
+        countDown(t-1, callback);
     }, 1000);
 }
 
-
-
 window.addEventListener("load", function(){
-    countDown(initialTime);
+    countDown(initialTime, redirectToLeetcode);
 });
